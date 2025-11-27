a19cb0678258b43f91cb3476788d0bbe47f5d4cc
Susumu Arimoto <susumu2934@gmail.com>
2025-11-27 20:56:47 +0900

    マイグレーションジョブを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_03_105147) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_27_115449) do
   create_table "cats", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 end
